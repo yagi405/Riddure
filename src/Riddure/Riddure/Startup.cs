dfb9b7a1d2c7daf8c788776cf4923760856b36e3
h-saito <h-saito@powersolutions.co.jp>
2020-12-02 11:05:34 +0900

    set default root

--- src/Riddure/Riddure/Startup.cs
+++ src/Riddure/Riddure/Startup.cs
@@ -31,10 +31,9 @@ namespace Riddure
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=ShortUrl}/{action=Index}/{id?}");
             });
         }
     }
